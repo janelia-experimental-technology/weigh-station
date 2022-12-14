e80730ae6ef961a120e540f447ecf297714067f5
Peter Polidoro <peter@polidoro.io>
2022-12-14 10:15:54 -0500

    Initial commit

---
+++ .manifest.scm
@@ -0,0 +1,12 @@
+;; This file is generated automatically from .metadata.org
+;; File edits may be overwritten!
+(specifications->manifest
+ '("rpi-imager"
+   "make"
+   "bash"
+   "git"
+   "emacs"
+   "emacs-org"
+   "emacs-ox-gfm"
+   "imagemagick"
+   "inkscape"))
